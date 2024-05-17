a0513c78add329c997a348125c0d1436afe5b299
AdamFcode <adamfeehely@gmail.com>
2024-05-17 14:04:41 +0000

    add meet troll function

--- run.py
+++ run.py
@@ -32,5 +32,35 @@ def path_select():
         time.sleep(3)
         return path_select()
 
+# User meets a Troll
+def meet_troll():
+    read_story("assets/text-files/meet-troll.txt")
+    story_choice = int(input("Please select 1, 2 or 3:\n"))
+    if story_choice == 1:
+        read_story("assets/text-files/defeat-troll.txt")
+        time.sleep(3)
+        riddle_path_one()
+    elif story_choice == 2:
+        read_story("assets/text-files/stun-troll.txt")
+        time.sleep(3)
+        stun_troll()
+    elif story_choice == 3:
+        read_story("assets/text-files/killed-by-troll.txt")
+        time.sleep(3)
+        read_story("assets/text-files/lose.txt")
+        play_again = int(input("Please select 1 or 2:\n"))
+        if play_again == 1:
+            adv_start()
+        elif play_again == 2:
+            exit()
+        else:
+            print("We shall take your inability to take instructions as a no...\n")
+            time.sleep(3)
+            exit()
+    else:
+        print("I am afraid that we cannot allow that selection. Please choose again.\n")
+        time.sleep(3)
+        meet_troll()
+
 adv_start()
 
